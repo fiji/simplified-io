542a8ec6ee7471185db495873b4326291eaa3555
Matthias Arzt <arzt@mpi-cbg.de>
2019-05-22 10:55:01 +0200

    ImageJIOUtilsTest: refactoring, remove code duplication
    Former-commit-id: f13a8af25c9a26f764891dcfe910c28fb38486a6

--- src/test/java/com/indago/io/ImageJIOUtilsTest.java
+++ src/test/java/com/indago/io/ImageJIOUtilsTest.java
@@ -3,9 +3,9 @@ package com.indago.io;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.function.Function;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,7 +17,8 @@ import ch.qos.logback.classic.LoggerContext;
 import net.imagej.ImgPlus;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
- public class ImageJIOUtilsTest { 
+
+public class ImageJIOUtilsTest {
 	private static String imagesPath = System.getProperty("user.dir") + File.separatorChar + "test-images";
 	private static Collection< File > tbp = null;
 	private static int ntot = 0;
@@ -66,92 +67,41 @@ import net.lingala.zip4j.exception.ZipException;
 	@SuppressWarnings( { "rawtypes" } )
 	@Test
 	public void testLoadImagesWithIJ1() {
-		System.out.println( "******* testLoadImagesWithIJ1" );
-		int cntr = 0;
-		for ( File file : tbp ) {
-			System.out.println( "Reading:" + file.getName() );
-			long start = System.nanoTime();
-			ImgPlus img1 = ImageJIOUtils.loadImageWithIJ1( file );
-			long finish = System.nanoTime();
-			long timeElapsed = finish - start;
-			if ( img1 != null ) {
-				System.out.println( "Time elapsed ij1 " + timeElapsed );
-				cntr++;
-				//if (!GraphicsEnvironment.isHeadless())ImageJFunctions.show((RandomAccessibleInterval) img1 );
-			}
-		}
-		System.out.println( ntot + " file found, " + cntr + " processed" );
+		testLoadImages( "testLoadImagesWithIJ1", file -> ImageJIOUtils.loadImageWithIJ1( file ) );
 	}
 
 	@SuppressWarnings( { "rawtypes" } )
 	@Test
 	public void testLoadImagesWithIJ() {
-		System.out.println( "******* testLoadImagesWithIJ" );
-		int cntr = 0;
-		for ( File file : tbp ) {
-			System.out.println( "Reading:" + file.getName() );
-			long start = System.nanoTime();
-			ImgPlus img2 = ImageJIOUtils.loadImageWithIJ( file, null );
-			long finish = System.nanoTime();
-			long timeElapsed = finish - start;
-			if ( img2 != null ) {
-				System.out.println( "Time elapsed scifio " + timeElapsed );
-				cntr++;
-				//if (!GraphicsEnvironment.isHeadless())ImageJFunctions.show((RandomAccessibleInterval) img2 );
-			}
-		}
-		System.out.println( ntot + " file found, " + cntr + " processed" );
+		testLoadImages( "testLoadImagesWithIJ", file -> ImageJIOUtils.loadImageWithIJ( file, null ) );
 	}
 
 	@SuppressWarnings( { "rawtypes" } )
 	@Test
 	public void testLoadImagesWithBioFormats() {
-		System.out.println( "******* testLoadImagesWithBioFormats" );
-		int cntr = 0;
-		for ( File file : tbp ) {
-			System.out.println( "Reading:" + file.getName() );
-			long start = System.nanoTime();
-			ImgPlus img2 = ImageJIOUtils.loadImageWithBioFormats( file );
-			long finish = System.nanoTime();
-			long timeElapsed = finish - start;
-			if ( img2 != null ) {
-				System.out.println( "Time elapsed " + timeElapsed );
-				cntr++;
-				//if (!GraphicsEnvironment.isHeadless()) ImageJFunctions.show((RandomAccessibleInterval) img2 );
-			}
-		}
-		System.out.println( ntot + " file found, " + cntr + " processed" );
+		testLoadImages( "testLoadImagesWithBioFormats", file -> ImageJIOUtils.loadImageWithBioFormats( file ) );
 	}
 
 	@SuppressWarnings( { "rawtypes" } )
 	@Test
 	public void testLoadImagesChooseIJ1() {
-		System.out.println( "******* testLoadImagesChooseIJ1" );
-		int cntr = 0;
-		for ( File file : tbp ) {
-			System.out.println( "Reading:" + file.getName() );
-			long start = System.nanoTime();
-			ImgPlus img2 = ImageJIOUtils.loadImage( file, true );
-			long finish = System.nanoTime();
-			long timeElapsed = finish - start;
-			if ( img2 != null ) {
-				System.out.println( "Time elapsed " + timeElapsed );
-				cntr++;
-				//if (!GraphicsEnvironment.isHeadless()) ImageJFunctions.show((RandomAccessibleInterval) img2 );
-			}
-		}
-		System.out.println( ntot + " file found, " + cntr + " processed" );
+		testLoadImages( "testLoadImagesChooseIJ1", file -> ImageJIOUtils.loadImage( file, true ) );
 	}
 
 	@SuppressWarnings( { "rawtypes" } )
 	@Test
 	public void testLoadImagesChooseIJ() {
-		System.out.println( "******* testLoadImagesChooseIJ" );
+		testLoadImages( "testLoadImagesChooseIJ", file -> ImageJIOUtils.loadImage( file, false ) );
+	}
+
+	private void testLoadImages( String title, Function< File, ImgPlus > loadImageFunction )
+	{
+		System.out.println( "******* " + title);
 		int cntr = 0;
 		for ( File file : tbp ) {
 			System.out.println( "Reading:" + file.getName() );
 			long start = System.nanoTime();
-			ImgPlus img2 = ImageJIOUtils.loadImage( file, false );
+			ImgPlus img2 = loadImageFunction.apply( file );
 			long finish = System.nanoTime();
 			long timeElapsed = finish - start;
 			if ( img2 != null ) {
