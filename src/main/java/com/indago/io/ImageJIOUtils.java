60a114db0855990f6caf009b5458167129ab05d6
Gabriella Turek <turek@mpi-cbg.de>
2019-06-04 14:49:12 +0200

    Added image writing methods

--- src/main/java/com/indago/io/ImageJIOUtils.java
+++ src/main/java/com/indago/io/ImageJIOUtils.java
@@ -1,20 +1,35 @@
 package com.indago.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.StringJoiner;
+
+import org.scijava.Context;
+import org.scijava.io.DefaultIOService;
+import org.scijava.service.Service;
+import org.scijava.util.FileUtils;
+
+import ij.IJ;
 import ij.ImagePlus;
 import ij.Macro;
+import ij.io.FileSaver;
 import ij.io.Opener;
 import ij.plugin.ImagesToStack;
+import ij.plugin.JpegWriter;
 import io.scif.SCIFIO;
 import loci.formats.FormatException;
 import loci.plugins.in.ImagePlusReader;
 import loci.plugins.in.ImportProcess;
 import loci.plugins.in.ImporterOptions;
 import net.imagej.Dataset;
+import net.imagej.DefaultDataset;
 import net.imagej.ImgPlus;
 import net.imagej.axis.Axes;
-import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
 import net.imagej.axis.DefaultLinearAxis;
+import net.imagej.types.DataTypeService;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.converter.Converters;
 import net.imglib2.converter.RealTypeConverters;
@@ -22,25 +37,16 @@ import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgView;
 import net.imglib2.img.display.imagej.ImgPlusViews;
+import net.imglib2.img.display.imagej.ImgToVirtualStack;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.util.Util;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.StringJoiner;
-
 public class ImageJIOUtils {
 
 	private static SCIFIO scifio;
-
-	private static SCIFIO getScifio() {
-		if( scifio == null )
-			scifio = new SCIFIO();
-		return scifio;
-	}
 
 	/**
 	 * Loads an image using ImageJ1, then wraps it into an ImgPlus object
@@ -111,93 +117,177 @@ public class ImageJIOUtils {
 	@SuppressWarnings( "rawtypes" )
 	public static ImgPlus loadImage( final File imgFile ) {
 
-		if( ! imgFile.exists() )
+		if ( !imgFile.exists() )
 			throw new ImageOpenException( "Image file doesn't exist: " + imgFile );
 
 		StringJoiner messages = new StringJoiner( "\n" );
 
 		try {
 			return ImageJIOUtils.loadImageWithIJ1( imgFile );
-		}
-		catch ( Exception e ) {
+		} catch ( Exception e ) {
 			messages.add( "ImageJ1 Exception: " + e.getMessage() );
 		}
 
 		try {
 			return ImageJIOUtils.loadImageWithSCIFIO( imgFile );
-		}
-		catch ( Exception e ) {
+		} catch ( Exception e ) {
 			messages.add( "SCIFIO Exception: " + e.getMessage() );
 		}
 
 		try {
 			return ImageJIOUtils.loadImageWithBioFormats( imgFile );
-		}
-		catch ( Exception e ) {
+		} catch ( Exception e ) {
 			messages.add( "BioFormats Exception: " + e.getMessage() );
 		}
 
-		throw new ImageOpenException( "Couldn't open image file: \"" + imgFile + "\"\n" +
-				"Exceptions:\n" + messages );
+		throw new ImageOpenException( "Couldn't open image file: \"" + imgFile + "\"\n" + "Exceptions:\n" + messages );
 	}
 
-	public static < T extends NativeType<T> > ImgPlus<T> loadImage( File file, T type )
-	{
+	public static < T extends NativeType< T > > ImgPlus< T > loadImage( File file, T type ) {
 		return convert( loadImage( file ), type );
 	}
 
-	private static < T extends NativeType<T> > ImgPlus<T> convert( ImgPlus image, T type )
-	{
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
+	public static < T extends NativeType< T > > ImgPlus< T > convert( ImgPlus image, T type ) {
 		Object imageType = Util.getTypeFromInterval( image );
-		if ( imageType.getClass().equals( type.getClass() ) )
-		{
+		if ( imageType.getClass().equals( type.getClass() ) ) {
 			return image;
-		}
-		else if( imageType instanceof RealType && type instanceof RealType )
-		{
+		} else if ( imageType instanceof RealType && type instanceof RealType ) {
 			return convertBetweenRealType( image, ( RealType ) type );
-		}
-		else if( imageType instanceof UnsignedByteType && type instanceof ARGBType )
-		{
+		} else if ( imageType instanceof UnsignedByteType && type instanceof ARGBType ) {
 			return convertUnsignedByteTypeToARGBType( image );
+		} else if ( imageType instanceof ARGBType && type instanceof RealType ) {
+			return convertARGBTypeToRealType( image, ( RealType ) type );
 		}
-		else if( imageType instanceof ARGBType && type instanceof RealType )
-		{
-			return convertARGBTypeToRealType( image, (RealType) type );
-		}
-		throw new IllegalStateException( "Cannot convert between given pixel types: " +
-				imageType.getClass().getSimpleName() + ", " + type.getClass().getSimpleName());
+		throw new IllegalStateException( "Cannot convert between given pixel types: " + imageType.getClass().getSimpleName() + ", " + type.getClass().getSimpleName() );
 	}
 
-	private static ImgPlus< ARGBType > convertUnsignedByteTypeToARGBType( ImgPlus<UnsignedByteType> image )
-	{
-		if( ImgPlusViews.canFuseColor( image ) 	)
+	/**
+	 * Saves the specified image to the specified file path.
+	 * Supported formats are "tif", ".jpg", ".png".
+	 * The specified image is saved as a "tif" if there is no extension.
+	 **/
+	public static void saveImage( ImgPlus< ? > img, String path ) {
+		saveImageWithIJ1( img, path );
+
+	}
+
+	/**
+	 * Saves the specified image to the specified file path.
+	 * Supported formats are "tif", ".jpg", ".png".
+	 * The specified image is saved as a "tif" if there is no extension.
+	 **/
+	public static void saveImageWithIJ1( ImgPlus< ? > img, String path ) {
+		String ext = FileUtils.getExtension( path ).toLowerCase();
+
+		if ( ext.isEmpty() ) {
+			ext = "tif";
+			path += ".tif";
+		}
+
+		ImagePlus wrappedImage = ImgToVirtualStack.wrap( img );
+
+		if ( ext.equalsIgnoreCase( "tif" ) || ext.equalsIgnoreCase( "tiff" ) ) {
+			FileSaver fs = new FileSaver( wrappedImage );
+			boolean ok;
+			if ( wrappedImage.getStackSize() > 1 )
+				ok = fs.saveAsTiffStack( path );
+			else
+				ok = fs.saveAsTiff( path );
+			if ( !ok )
+				throw new ImageWriteException( "Failed to save image to " + path );
+			else
+				return;
+
+		} else if ( ext.equalsIgnoreCase( "jpg" ) || ext.equalsIgnoreCase( "jpeg" ) ) {
+
+			String error = JpegWriter.save( wrappedImage, path, FileSaver.getJpegQuality() );
+
+			if ( error != null )
+				throw new ImageWriteException( "Error while saving tif image " + error );
+			else
+				return;
+
+		} else if ( ext.equalsIgnoreCase( "png" ) ) {
+
+			IJ.run( wrappedImage, "PNG...", "save=" + path );
+
+		} else {
+			throw new ImageWriteException( "Unsupported format \"" + ext + "\"" );
+		}
+	}
+
+	/**
+	 * Saves the specified image to the specified file path.
+	 * Supported formats are "tif", ".jpg", ".png".
+	 * The specified image is saved as a "tif" if there is no extension.
+	 * This method is a lot slower than the IJ1 method, it is here for
+	 * comparison....
+	 **/
+	@SuppressWarnings( "unchecked" )
+	public static void saveImageWithSCIFIO( ImgPlus< ? > img, String path ) {
+		String ext = FileUtils.getExtension( path ).toLowerCase();
+
+		if ( ext.isEmpty() ) {
+			ext = "tif";
+			path += ".tif";
+		}
+
+		try {
+			Collection< Class< ? extends Service > > services =
+					new ArrayList< Class< ? extends Service > >();
+			services.add( DefaultIOService.class );
+			services.add( DataTypeService.class );
+			DefaultDataset dataset =
+					new DefaultDataset( new Context( services ), ( ImgPlus< ? extends RealType< ? > > ) img );
+			getScifio().datasetIO().save( dataset, path );
+
+		} catch ( IOException e ) {
+
+			throw new ImageWriteException( "Incompatible type exception: " + e.getMessage() );
+		}
+	}
+
+	private static ImgPlus< ARGBType >
+			convertUnsignedByteTypeToARGBType( ImgPlus< UnsignedByteType > image ) {
+		if ( ImgPlusViews.canFuseColor( image ) )
 			return ImgPlusViews.fuseColor( image );
-		RandomAccessibleInterval< ARGBType > convertedRAI = Converters.convertRAI( image.getImg(),
-				(i, o) -> { int value = i.get() ; o.set( ARGBType.rgba( value, value, value, 255 ) ); },
+		RandomAccessibleInterval< ARGBType > convertedRAI = Converters.convertRAI(
+				image.getImg(),
+				( i, o ) -> {
+					int value = i.get();
+					o.set( ARGBType.rgba( value, value, value, 255 ) );
+				},
 				new ARGBType() );
 		Img< ARGBType > convertedImg = ImgView.wrap( convertedRAI, null );
 		return new ImgPlus<>( convertedImg, image );
 	}
 
-	private static < T extends NativeType< T > > ImgPlus< T > convertBetweenRealType( ImgPlus image, RealType type )
-	{
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
+	private static < T extends NativeType< T > > ImgPlus< T >
+			convertBetweenRealType( ImgPlus image, RealType type ) {
 		RandomAccessibleInterval< T > convertedRAI = RealTypeConverters.convert( image, type );
 		Img< T > convertedImg = ImgView.wrap( convertedRAI, null );
 		return new ImgPlus<>( convertedImg, image );
 	}
 
-	private static < T extends RealType<T> > ImgPlus<T> convertARGBTypeToRealType( ImgPlus<ARGBType> image, T type )
-	{
-		RandomAccessibleInterval< T > convertedRAI = RealTypeConverters.convert( Converters.argbChannels( image ), type );
+	private static < T extends RealType< T > > ImgPlus< T >
+			convertARGBTypeToRealType( ImgPlus< ARGBType > image, T type ) {
+		RandomAccessibleInterval< T > convertedRAI =
+				RealTypeConverters.convert( Converters.argbChannels( image ), type );
 		Img< T > convertedImg = ImgView.wrap( convertedRAI, null );
 		int n = image.numDimensions();
 		CalibratedAxis[] axis = new CalibratedAxis[ n + 1 ];
-		for ( int i = 0; i < n; i++ )
-		{
-			axis[i]	= image.axis( i );
+		for ( int i = 0; i < n; i++ ) {
+			axis[ i ] = image.axis( i );
 		}
-		axis[n] = new DefaultLinearAxis( Axes.CHANNEL );
+		axis[ n ] = new DefaultLinearAxis( Axes.CHANNEL );
 		return new ImgPlus<>( convertedImg, image.getName(), axis );
+	}
+
+	private static SCIFIO getScifio() {
+		if ( scifio == null )
+			scifio = new SCIFIO();
+		return scifio;
 	}
 }
