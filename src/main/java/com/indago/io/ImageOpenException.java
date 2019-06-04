999604e46893c7b7a2d2bc2b5709dfab4206b0af
Gabriella Turek <turek@mpi-cbg.de>
2019-06-04 14:49:12 +0200

    Added image writing methods
    Former-commit-id: a8f8a22963dc4b793798fd69e84eb78d6a920177

--- src/main/java/com/indago/io/ImageOpenException.java
+++ src/main/java/com/indago/io/ImageOpenException.java
@@ -1,9 +1,13 @@
 package com.indago.io;
 
-import java.io.IOException;
-
+/**
+ * Runtime exception for fatal errors encountered while
+ * opening an image (e.g. IOException, FileNotFoundException, etc)
+ */
 public class ImageOpenException extends RuntimeException
 {
+	private static final long serialVersionUID = 349763419228666057L;
+
 	public ImageOpenException( Exception cause ) {
 		super( cause );
 	}
