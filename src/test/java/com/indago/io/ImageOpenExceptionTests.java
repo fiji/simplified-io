551436c744dab411b495a8902a4dd43be428edeb
Matthias Arzt <arzt@mpi-cbg.de>
2019-06-18 15:10:12 +0200

    Rename loadImage to openImage, make library dependent methods private

--- src/test/java/com/indago/io/ImageOpenExceptionTests.java
+++ src/test/java/com/indago/io/ImageOpenExceptionTests.java
@@ -10,7 +10,7 @@ public class ImageOpenExceptionTests {
 	@Test( expected = ImageOpenException.class )
 	public void testMissingFile() throws IOException {
 		File file = File.createTempFile( "image-", ".tif" );
-		ImageJIOUtils.loadImage( file );
+		ImageJIOUtils.openImage( file );
 	}
 
 	@Test( expected = ImageOpenException.class )
@@ -18,6 +18,6 @@ public class ImageOpenExceptionTests {
 		File file = File.createTempFile( "image-", ".tif" );
 		file.createNewFile();
 		file.deleteOnExit();
-		ImageJIOUtils.loadImage( file );
+		ImageJIOUtils.openImage( file );
 	}
 }
