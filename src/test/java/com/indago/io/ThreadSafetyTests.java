77823f83fec9a6b2aa17d371c5e0dc4b0785e97c
Matthias Arzt <arzt@mpi-cbg.de>
2019-06-18 15:05:29 +0200

    Improve ThreadSaftyTest
    Use different images, and throw an exception on failure.

--- src/test/java/com/indago/io/ThreadSafetyTests.java
+++ src/test/java/com/indago/io/ThreadSafetyTests.java
@@ -1,109 +1,127 @@
 package com.indago.io;
 
+import net.imagej.ImgPlus;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.test.ImgLib2Assert;
+import net.imglib2.test.RandomImgs;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import net.imagej.ImgPlus;
-import net.imagej.ops.create.imgPlus.DefaultCreateImgPlus;
-import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.test.ImgLib2Assert;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
-
+/**
+ * Write and read a thousand different images in parallel,
+ * and check if the content is correct. This is done to
+ * ensure that threads writing and reading at the same
+ * time don't interfere with each other.
+ */
 public class ThreadSafetyTests {
 
-	private static ImgPlus< ? > imgPlus = null;
-	private static List< File > files;
+	private Map< ImgPlus< ? >, File > imagesAndFiles = initImageAndFiles();
 
-	@SuppressWarnings( { "unchecked", "rawtypes" } )
-	@BeforeClass
-	public static void init() {
-		ArrayImgFactory< UnsignedByteType > imageFactory =
-				new ArrayImgFactory( new UnsignedByteType() );
-		Img< UnsignedByteType > img = imageFactory.create( new long[] { 400, 320 } );
-		imgPlus = new DefaultCreateImgPlus().calculate( img );
-
-		files = new ArrayList< File >( 1000 );
-		for ( int i = 1; i < 6; i++ ) {
-			for ( int j = 1; j < 201; j++ ) {
-				File outputFile = new File( "/tmp/threadtest" + i + j + ".tif" );
+	private Map<ImgPlus<?>,File> initImageAndFiles()
+	{
+		try
+		{
+			Map< ImgPlus< ? >, File > result = new HashMap<>();
+			for ( int i = 0; i < 1000; i++ ) {
+				File outputFile = File.createTempFile( "testTreadSafety",".tif" );
 				outputFile.deleteOnExit();
-				files.add( outputFile );
+				result.put( createImage(i), outputFile );
 			}
+			return result;
+		}
+		catch ( IOException e )
+		{
+			throw new RuntimeException( e );
 		}
 	}
 
-	@Test
-	public void test() throws IOException {
-		System.out.println( "Run write threads" );
-		ExecutorService executor = Executors.newFixedThreadPool( 5 );
-		for ( int i = 0; i < 5; i++ ) {
-			Runnable worker = new MultiImageWriter( imgPlus, files.subList(i*200, (i + 1)*200 - 1  ));
-			executor.execute( worker );
-		}
-		executor.shutdown();
-		// Wait until all threads are finish
-		while ( !executor.isTerminated() ) {}
-		System.out.println( "Finished all writing threads, run read threads" );
+	private ImgPlus< ? > createImage( int i )
+	{
+		Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 400, 320 );
+		ImgPlus< UnsignedByteType > imgPlus = new ImgPlus<>( img );
+		RandomImgs.seed( i ).randomize( imgPlus );
+		return imgPlus;
+	}
 
-		executor = Executors.newFixedThreadPool( 5 );
-		for ( int i = 0; i < 5; i++ ) {
-			Runnable worker = new MultiImageReader( imgPlus, files.subList(i*200, (i + 1)*200 - 1  ) );
-			executor.execute( worker );
-		}
-		executor.shutdown();
-		// Wait until all threads are finish
-		while ( !executor.isTerminated() ) {}
+	@Test
+	public void test() throws IOException, ExecutionException, InterruptedException
+	{
+		System.out.println( "Run write threads" );
+		writeParallel();
+		System.out.println( "Finished all writing threads, run read threads" );
+		readParallel();
 		System.out.println( "Finished all reading threads, test terminated successfully" );
 
+	}
+
+	private void writeParallel()
+	{
+		ExecutorService executor = Executors.newFixedThreadPool( 5 );
+		for ( Map.Entry< ImgPlus< ? >, File > entry : imagesAndFiles.entrySet() ) {
+			Runnable worker = new MultiImageWriter( entry.getKey(), entry.getValue() );
+			executor.execute( worker );
+		}
+		executor.shutdown();
+		// Wait until all tasks are finish
+		while ( !executor.isTerminated() ) {}
+	}
+
+	private void readParallel() throws ExecutionException, InterruptedException
+	{
+		ExecutorService executor = Executors.newFixedThreadPool( 5 );
+		List< Future< ? >> futures = new ArrayList<>(  );
+		imagesAndFiles.forEach( ( image, file ) -> {
+			Runnable worker = new MultiImageReader( image, file );
+			futures.add( executor.submit( worker ) );
+		} );
+		// Wait until all tasks are finish
+		for( Future< ? > future : futures )
+			future.get();
+		executor.shutdown();
 	}
 }
 
 class MultiImageWriter implements Runnable {
 
-	private Collection< File > files = null;
-	private ImgPlus< ? > imgPlus = null;
+	private final File file;
+	private final ImgPlus< ? > imgPlus;
 
-	public MultiImageWriter( ImgPlus< ? > imgPlus, List<File> files ) {
+	public MultiImageWriter( ImgPlus< ? > imgPlus, File files ) {
 		this.imgPlus = imgPlus;
-		this.files = files;
+		this.file = files;
 	}
 
 	public void run() {
-		for ( File file : files ) {
-			String outputFilePath = file.getPath();
-			ImageJIOUtils.saveImage( imgPlus, outputFilePath );
-		}
+		String outputFilePath = file.getPath();
+		ImageJIOUtils.saveImage( imgPlus, outputFilePath );
 	}
 }
 
 class MultiImageReader implements Runnable {
 
-	private Collection< File > files = null;
-	private ImgPlus< ? > imgPlus = null;
+	private final File file;
+	private final ImgPlus< ? > imgPlus;
 
-	public MultiImageReader( ImgPlus< ? > imgPlus, List<File> files) {
+	public MultiImageReader( ImgPlus< ? > imgPlus, File file ) {
 		this.imgPlus = imgPlus;
-		this.files = files;
+		this.file = file;
 	}
 
 	@SuppressWarnings( { "rawtypes", "unchecked" } )
 	public void run() {
-		for ( File file : files ) {
-			ImgPlus< ? > readImage = ImageJIOUtils.loadImage( file );
-			try {
-				ImgLib2Assert.assertImageEquals( ( Img ) readImage.getImg(), ( Img ) imgPlus.getImg() );
-			} catch ( ImageWriteException | AssertionError e ) {
-				System.out.println( e.getMessage() );
-			}
-		}
+		ImgPlus< ? > readImage = ImageJIOUtils.loadImage( file );
+		ImgLib2Assert.assertImageEquals( ( Img ) readImage.getImg(), ( Img ) imgPlus.getImg() );
 	}
 }
